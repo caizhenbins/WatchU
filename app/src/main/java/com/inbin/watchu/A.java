4cf837c4b495123bdebe7f39bc7db9010f8376f1
caizhenbin <caizhenbins@163.com>
2017-05-27 11:16:06 +0800

    A提交

---
+++ app/src/main/java/com/inbin/watchu/A.java
@@ -0,0 +1,11 @@
+package com.inbin.watchu;
+
+/**
+ * Created by InBin time: 2017/5/27.
+ * Author: caizhenbin
+ * Email: caizhenbins@163.com
+ * PackName: com.inbin.watchu
+ */
+
+public class A {
+}
